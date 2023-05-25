30bc49116de7429148a09cb234ae6a0f6452760c
Shane Stevens <102969628+BEASTxk9@users.noreply.github.com>
2023-05-25 13:55:31 +0200

    update

---
+++ JS/contact.js
@@ -0,0 +1,15 @@
+document.getElementById("contact").innerHTML = `
+<div class="container">
+<div class="row justify-content-center text-center">
+    <div class="col-sm-4">
+        <p>Contact: +123456789</p>
+    </div>
+    <div class="col-sm-4">
+        <p>Email: roastedbeansxk9@gmail.com</p>
+    </div>
+    <div class="col-sm-4">
+        <p>socials: not sure</p>
+    </div>
+</div>
+</div>
+`
